267bd21026e7f24506d7a60647d08122338e0237
Antonio Francisco Silva Gomes <73271272+AntonioGomes42@users.noreply.github.com>
2020-10-22 02:36:03 -0300

    Hello Norris, font code

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img 
+        src="https://giffiles.alphacoders.com/125/1250.gif"
+        alt="chuck"
+        />
+        <div>
+           #_#
+        </div>
+        Hello Motherfuckers!
+      </header>
+    </div>
+  );
+}
+
+export default App;
