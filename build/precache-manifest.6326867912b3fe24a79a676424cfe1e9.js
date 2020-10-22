2f729450930699873cd640c9c57e2696c83ef5fe
Antonio Francisco Silva Gomes <73271272+AntonioGomes42@users.noreply.github.com>
2020-10-22 02:39:28 -0300

    Build, hello norris

---
+++ build/precache-manifest.6326867912b3fe24a79a676424cfe1e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9529425b5be4df21fe796028a5f0629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d5d0fdadd6646d7da74",
+    "url": "/static/css/main.5e308da4.chunk.css"
+  },
+  {
+    "revision": "ee3bb440075adad9262a",
+    "url": "/static/js/2.8d564248.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.8d564248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5d0fdadd6646d7da74",
+    "url": "/static/js/main.f8415014.chunk.js"
+  },
+  {
+    "revision": "1550bc5cb6224d84350f",
+    "url": "/static/js/runtime-main.74ac9606.js"
+  }
+]);
